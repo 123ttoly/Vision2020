2e46faf2e72174ab898df11cdc3ec92f0c10e11a
123ttoly <59716640+123ttoly@users.noreply.github.com>
2020-01-25 12:39:20 -0600

    Add files via upload

--- multiCameraServer.py
+++ multiCameraServer.py
@@ -62,11 +62,37 @@ configFile = "/boot/frc.json"
 
 class CameraConfig: pass
 
-team = None
+team = 2040
 server = False
 cameraConfigs = []
 switchedCameraConfigs = []
 cameras = []
+
+__hsv_threshold_hue = [59.89208633093525, 100.13651877133107]
+__hsv_threshold_saturation = [139.88309352517987, 255.0]
+__hsv_threshold_value = [162.81474820143885, 255.0]
+
+hsv_threshold_output = None
+
+__find_contours_input =  hsv_threshold_output
+__find_contours_external_only = False
+
+find_contours_output = None
+
+__filter_contours_contours =  find_contours_output
+__filter_contours_min_area = 16.0
+__filter_contours_min_perimeter = 0
+__filter_contours_min_width = 0
+__filter_contours_max_width = 1000.0
+__filter_contours_min_height = 0
+__filter_contours_max_height = 1000
+__filter_contours_solidity = [0.0, 33.44709897610921]
+__filter_contours_max_vertices = 1000000.0
+__filter_contours_min_vertices = 10.0
+__filter_contours_min_ratio = 0
+__filter_contours_max_ratio = 1000
+
+filter_contours_output = None
 
 def parseError(str):
     """Report parse error."""
@@ -208,9 +234,53 @@ def startSwitchedCamera(config):
         ntcore.constants.NT_NOTIFY_IMMEDIATE |
         ntcore.constants.NT_NOTIFY_NEW |
         ntcore.constants.NT_NOTIFY_UPDATE)
-
     return server
 
+def __hsv_threshold(input, hue, sat, val):
+    out = cv2.cvtColor(input, cv2.COLOR_BGR2HSV)
+    return cv2.inRange(out, (hue[0], sat[0], val[0]),  (hue[1], sat[1], val[1]))
+
+def __find_contours(input, external_only):
+    if(external_only):
+        mode = cv2.RETR_EXTERNAL
+    else:
+        mode = cv2.RETR_LIST
+        method = cv2.CHAIN_APPROX_SIMPLE
+        im2, contours, hierarchy =cv2.findContours(input, mode=mode, method=method)
+    return contours
+
+def __filter_contours(input_contours, min_area, min_perimeter, min_width, max_width,
+                    min_height, max_height, solidity, max_vertex_count, min_vertex_count,
+                    min_ratio, max_ratio):
+    output = []
+    for contour in input_contours:
+        x,y,w,h = cv2.boundingRect(contour)
+        if (w < min_width or w > max_width):
+            continue
+        if (h < min_height or h > max_height):
+            continue
+        area = cv2.contourArea(contour)
+        if (area < min_area):
+            continue
+        if (cv2.arcLength(contour, True) < min_perimeter):
+            continue
+        hull = cv2.convexHull(contour)
+        solid = 100 * area / cv2.contourArea(hull)
+        if (solid < solidity[0] or solid > solidity[1]):
+            continue
+        if (len(contour) < min_vertex_count or len(contour) > max_vertex_count):
+            continue
+        ratio = (float)(w) / h
+        if (ratio < min_ratio or ratio > max_ratio):
+            continue
+        d = float((3.25 * 640)/(2 * w * 30.5))
+        output.append(contour)
+        return output
+        return d
+        return x
+        return y
+    
+        
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
         configFile = sys.argv[1]
@@ -236,131 +306,23 @@ if __name__ == "__main__":
     for config in switchedCameraConfigs:
         startSwitchedCamera(config)
 
+    cap = cv2.VideoCapture(0)
+    cap.set(10, 0.1)
+
     # loop forever
     while True:
-        time.sleep(10)
-        def __init__(self):
-        """initializes all values to presets or None if need to be set
-        """
-         self.__hsv_threshold_hue = [59.89208633093525, 100.13651877133107]
-         self.__hsv_threshold_saturation = [139.88309352517987, 255.0]
-         self.__hsv_threshold_value = [162.81474820143885, 255.0]
 
-         self.hsv_threshold_output = None
+        ret, frame = cap.read()
 
-         self.__find_contours_input = self.hsv_threshold_output
-         self.__find_contours_external_only = False
+        # Step HSV_Threshold0:
+        #__hsv_threshold_input = cameras[0]
+        (hsv_threshold_output) =  __hsv_threshold( frame,  __hsv_threshold_hue,  __hsv_threshold_saturation,  __hsv_threshold_value)
 
-         self.find_contours_output = None
-
-         self.__filter_contours_contours = self.find_contours_output
-         self.__filter_contours_min_area = 16.0
-         self.__filter_contours_min_perimeter = 0
-         self.__filter_contours_min_width = 0
-         self.__filter_contours_max_width = 1000.0
-         self.__filter_contours_min_height = 0
-         self.__filter_contours_max_height = 1000
-         self.__filter_contours_solidity = [0.0, 33.44709897610921]
-         self.__filter_contours_max_vertices = 1000000.0
-         self.__filter_contours_min_vertices = 10.0
-         self.__filter_contours_min_ratio = 0
-         self.__filter_contours_max_ratio = 1000
-
-         self.filter_contours_output = None
-
-
-        def process(self, source0):
-        """
-        Runs the pipeline and sets all outputs to new values.
-        """
-            # Step HSV_Threshold0:
-            self.__hsv_threshold_input = source0
-            (self.hsv_threshold_output) = self.__hsv_threshold(self.__hsv_threshold_input, self.__hsv_threshold_hue, self.__hsv_threshold_saturation, self.__hsv_threshold_value)
-
-            # Step Find_Contours0:
-            self.__find_contours_input = self.hsv_threshold_output
-            (self.find_contours_output) = self.__find_contours(self.__find_contours_input, self.__find_contours_external_only)
-
-            # Step Filter_Contours0:
-            self.__filter_contours_contours = self.find_contours_output
-            (self.filter_contours_output) = self.__filter_contours(self.__filter_contours_contours, self.__filter_contours_min_area, self.__filter_contours_min_perimeter, self.__filter_contours_min_width, self.__filter_contours_max_width, self.__filter_contours_min_height, self.__filter_contours_max_height, self.__filter_contours_solidity, self.__filter_contours_max_vertices, self.__filter_contours_min_vertices, self.__filter_contours_min_ratio, self.__filter_contours_max_ratio)
-
-
-        @staticmethod
-        def __hsv_threshold(input, hue, sat, val):
-            """Segment an image based on hue, saturation, and value ranges.
-            Args:
-                input: A BGR numpy.ndarray.
-                hue: A list of two numbers the are the min and max hue.
-                sat: A list of two numbers the are the min and max saturation.
-                lum: A list of two numbers the are the min and max value.
-            Returns:
-                A black and white numpy.ndarray.
-            """
-            out = cv2.cvtColor(input, cv2.COLOR_BGR2HSV)
-            return cv2.inRange(out, (hue[0], sat[0], val[0]),  (hue[1], sat[1], val[1]))
-
-        @staticmethod
-        def __find_contours(input, external_only):
-            """Sets the values of pixels in a binary image to their distance to the nearest black pixel.
-            Args:
-                input: A numpy.ndarray.
-                external_only: A boolean. If true only external contours are found.
-            Return:
-                A list of numpy.ndarray where each one represents a contour.
-            """
-            if(external_only):
-                mode = cv2.RETR_EXTERNAL
-            else:
-                mode = cv2.RETR_LIST
-            method = cv2.CHAIN_APPROX_SIMPLE
-            im2, contours, hierarchy =cv2.findContours(input, mode=mode, method=method)
-            return contours
-
-        @staticmethod
-        def __filter_contours(input_contours, min_area, min_perimeter, min_width, max_width,
-                            min_height, max_height, solidity, max_vertex_count, min_vertex_count,
-                            min_ratio, max_ratio):
-            """Filters out contours that do not meet certain criteria.
-            Args:
-                input_contours: Contours as a list of numpy.ndarray.
-                min_area: The minimum area of a contour that will be kept.
-                min_perimeter: The minimum perimeter of a contour that will be kept.
-                min_width: Minimum width of a contour.
-                max_width: MaxWidth maximum width.
-                min_height: Minimum height.
-                max_height: Maximimum height.
-                solidity: The minimum and maximum solidity of a contour.
-                min_vertex_count: Minimum vertex Count of the contours.
-                max_vertex_count: Maximum vertex Count.
-                min_ratio: Minimum ratio of width to height.
-                max_ratio: Maximum ratio of width to height.
-            Returns:
-                Contours as a list of numpy.ndarray.
-            """
-            output = []
-            for contour in input_contours:
-                x,y,w,h = cv2.boundingRect(contour)
-                if (w < min_width or w > max_width):
-                    continue
-                if (h < min_height or h > max_height):
-                    continue
-                area = cv2.contourArea(contour)
-                if (area < min_area):
-                    continue
-                if (cv2.arcLength(contour, True) < min_perimeter):
-                    continue
-             hull = cv2.convexHull(contour)
-                solid = 100 * area / cv2.contourArea(hull)
-                if (solid < solidity[0] or solid > solidity[1]):
-                    continue
-                if (len(contour) < min_vertex_count or len(contour) > max_vertex_count):
-                    continue
-                ratio = (float)(w) / h
-                if (ratio < min_ratio or ratio > max_ratio):
-                    continue
-                d = float((3.25 * 640)/(2 * w * 30.5))
-                output.append(contour)
-            return output d x y
+        # Step Find_Contours0:
+        __find_contours_input =  hsv_threshold_output
+        ( find_contours_output) =  __find_contours( __find_contours_input,  __find_contours_external_only)
+ 
+        # Step Filter_Contours0:
+        __filter_contours_contours =  find_contours_output
+        ( filter_contours_output) =  __filter_contours( __filter_contours_contours,  __filter_contours_min_area,  __filter_contours_min_perimeter,  __filter_contours_min_width,  __filter_contours_max_width,  __filter_contours_min_height,  __filter_contours_max_height,  __filter_contours_solidity,  __filter_contours_max_vertices,  __filter_contours_min_vertices,  __filter_contours_min_ratio,  __filter_contours_max_ratio)
         print(d, x, y)
-        
